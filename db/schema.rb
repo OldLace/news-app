580bc5d6a920679a01a85ddc1a914ef9865f4d2c
Paul Gelot <pgelot@gmail.com>
2017-12-19 22:53:24 -0500

    new model for user comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215192433) do
+ActiveRecord::Schema.define(version: 20171220035206) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.string "title", null: false
